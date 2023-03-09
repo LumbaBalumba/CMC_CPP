ecc190c62bcf9300e217fd849e3695a9e3fd6171
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2023-03-09 23:28:05 +0300

    3-4 (better) (better)

--- 3/4.cpp
+++ 3/4.cpp
@@ -4,22 +4,23 @@
 #include <cmath>
 #include <vector>
 #include <string>
+#include <iostream>
 
 int
 main(int argc, char **argv)
 {
-    numbers::complex C(argv[1]);
-    double R = fabs(strtod(argv[2], nullptr));
-    int N = (int) strtol(argv[3], nullptr, 10);
+    const numbers::complex C(argv[1]);
+    const double R = fabs(strtod(argv[2], nullptr));
+    const int N = (int) strtol(argv[3], nullptr, 10);
     std::vector<std::string> func = {};
     for (int i = 4; i < argc; ++i) {
         func.emplace_back(argv[i]);
     }
     numbers::complex sum = 0;
+    const double alpha = 2 * std::numbers::pi / N;
     for (int i = 0; i < N; ++i) {
-        double alpha = 2 * std::numbers::pi / N;
-        numbers::complex z = C + R * numbers::complex(cos(alpha * i + alpha / 2), sin(alpha * i + alpha / 2));
-        sum += numbers::eval(func, z) * fabs(R * alpha);
+        numbers::complex z = C + R * numbers::complex(cos(alpha * i), sin(alpha * i));
+        sum += numbers::eval(func, z) * R * alpha;
     }
     std::cout << sum.to_string() << std::endl;
 }
