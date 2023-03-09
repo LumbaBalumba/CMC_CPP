63554a1531c9ddc03dbb3630b1762bab38c39480
i3alumba <vlad.lazar.ik528@yandex.ru>
2023-03-09 15:23:21 +0300

    2-6 (again) (again) (again)

--- 2/6.cpp
+++ 2/6.cpp
@@ -1,40 +1,50 @@
 #include <iostream>
 #include <string>
 
-class StringView {
+class StringView
+{
     char *value;
     size_t _size;
 
 public:
     StringView() = default;
 
-    StringView(const std::string &s, size_t begin = 0, size_t count = std::string::npos) {
+    StringView(const std::string &s, size_t begin = 0, size_t count = std::string::npos)
+    {
         value = (char *) s.c_str() + begin;
         _size = s.size();
     }
 
+    StringView &
+    operator=(const StringView &other) = delete;
+
     char &
-    operator[](size_t index) const {
+    operator[](size_t index) const
+    {
         return value[index];
     }
 
-    StringView(const StringView &sv, size_t begin = 0, size_t count = std::string::npos) {
+    StringView(const StringView &sv, size_t begin = 0, size_t count = std::string::npos)
+    {
         value = sv.value + begin;
         _size = count;
     }
 
     [[nodiscard]] size_t
-    size() const {
+    size() const
+    {
         return _size;
     }
 
     [[nodiscard]] size_t
-    length() const {
+    length() const
+    {
         return _size;
     }
 
     [[nodiscard]] std::string
-    str(size_t begin = 0, size_t count = std::string::npos) const {
+    str(size_t begin = 0, size_t count = std::string::npos) const
+    {
         std::string res{};
         for (size_t i = 0; i < count; ++i) {
             res += value[begin + i];
@@ -43,71 +53,85 @@ public:
     }
 
     friend auto
-    operator<=>(const StringView &left, const StringView &right) {
+    operator<=>(const StringView &left, const StringView &right)
+    {
         return std::string(left.value) <=> std::string(right.value);
     }
 
     friend auto
-    operator==(const StringView &left, const StringView &right) {
+    operator==(const StringView &left, const StringView &right)
+    {
         return std::string(left.value) == std::string(right.value);
     }
 
-    class iterator {
+    class iterator
+    {
     protected:
         char *iter;
     public:
-        iterator(char *ptr) {
+        iterator(char *ptr)
+        {
             iter = ptr;
         }
 
-        iterator(const iterator &other) {
+        iterator(const iterator &other)
+        {
             iter = other.iter;
         }
 
         char &
-        operator*() {
+        operator*()
+        {
             return *iter;
         }
 
         virtual iterator &
-        operator++() {
+        operator++()
+        {
             ++iter;
             return *this;
         }
 
         iterator
-        operator++(int) {
-            return {iter++};
+        operator++(int)
+        {
+            return { iter++ };
         }
 
         virtual iterator &
-        operator--() {
+        operator--()
+        {
             --iter;
             return *this;
         }
 
         iterator
-        operator--(int) {
-            return {iter--};
+        operator--(int)
+        {
+            return { iter-- };
         }
 
         iterator
-        operator+(ssize_t index) const {
-            return {iter + index};
+        operator+(ssize_t index) const
+        {
+            return { iter + index };
         }
 
         iterator
-        operator-(ssize_t index) const {
-            return {iter - index};
+        operator-(ssize_t index) const
+        {
+            return { iter - index };
         }
 
         virtual iterator &
-        operator+=(ssize_t index) {
+        operator+=(ssize_t index)
+        {
             return *this = *this + index;
         }
 
         virtual iterator &
-        operator-=(ssize_t index) {
+        operator-=(ssize_t index)
+        {
             return *this = *this - index;
         }
 
@@ -115,24 +139,29 @@ public:
         operator<=>(const iterator &other) const = default;
 
         bool
-        operator==(const iterator &other) const {
+        operator==(const iterator &other) const
+        {
             return iter == other.iter;
         }
 
         friend std::ostream &
-        operator<<(std::ostream &out, const iterator &i) {
+        operator<<(std::ostream &out, const iterator &i)
+        {
             return out << *i.iter;
         }
     };
 
-    class const_iterator : protected iterator {
+    class const_iterator : protected iterator
+    {
     public:
-        const_iterator(char *ptr) : iterator(ptr) {}
+        const_iterator(char *ptr) : iterator(ptr)
+        {}
 
         const_iterator(const const_iterator &other) = default;
 
         char
-        operator*() {
+        operator*()
+        {
             return *iterator::iter;
         }
 
@@ -146,9 +175,11 @@ public:
         using iterator::operator==;
     };
 
-    class reverse_iterator : protected iterator {
+    class reverse_iterator : protected iterator
+    {
     public:
-        reverse_iterator(char *ptr) : iterator(ptr) {}
+        reverse_iterator(char *ptr) : iterator(ptr)
+        {}
 
         reverse_iterator(const reverse_iterator &other) = default;
 
@@ -156,44 +187,62 @@ public:
         using iterator::operator==;
         using iterator::operator<=>;
 
-        reverse_iterator &operator++() override {
+        reverse_iterator &
+        operator++() override
+        {
             --iterator::iter;
             return *this;
         }
 
-        reverse_iterator &operator--() override {
+        reverse_iterator &
+        operator--() override
+        {
             ++iterator::iter;
             return *this;
         }
 
-        reverse_iterator operator++(int) {
-            return {iterator::iter--};
+        reverse_iterator
+        operator++(int)
+        {
+            return { iterator::iter-- };
         }
 
-        reverse_iterator operator--(int) {
-            return {iterator::iter++};
+        reverse_iterator
+        operator--(int)
+        {
+            return { iterator::iter++ };
         }
 
-        reverse_iterator operator+(ssize_t other) const {
-            return {iterator::iter - other};
+        reverse_iterator
+        operator+(ssize_t other) const
+        {
+            return { iterator::iter - other };
         }
 
-        reverse_iterator operator-(ssize_t other) const {
-            return {iterator::iter + other};
+        reverse_iterator
+        operator-(ssize_t other) const
+        {
+            return { iterator::iter + other };
         }
 
-        reverse_iterator &operator+=(ssize_t other) override {
+        reverse_iterator &
+        operator+=(ssize_t other) override
+        {
             return *this = *this - other;
         }
 
-        reverse_iterator &operator-=(ssize_t other) override {
+        reverse_iterator &
+        operator-=(ssize_t other) override
+        {
             return *this = *this + other;
         }
     };
 
-    class const_reverse_iterator : protected reverse_iterator, protected const_iterator {
+    class const_reverse_iterator : protected reverse_iterator, protected const_iterator
+    {
     public:
-        const_reverse_iterator(char *iter) : reverse_iterator(iter), const_iterator(iter) {}
+        const_reverse_iterator(char *iter) : reverse_iterator(iter), const_iterator(iter)
+        {}
 
         const_reverse_iterator(const const_reverse_iterator &other) = default;
 
@@ -209,17 +258,20 @@ public:
     };
 
     [[nodiscard]] iterator
-    begin() const {
-        return {value};
+    begin() const
+    {
+        return { value };
     }
 
     [[nodiscard]] iterator
-    end() const {
-        return {value + _size};
+    end() const
+    {
+        return { value + _size };
     }
 
     [[nodiscard]] char &
-    at(size_t index) const {
+    at(size_t index) const
+    {
         if (index <= _size) {
             return *(value + index);
         } else {
@@ -228,32 +280,38 @@ public:
     }
 
     [[nodiscard]] const_iterator
-    cbegin() const {
-        return {value};
+    cbegin() const
+    {
+        return { value };
     }
 
     [[nodiscard]] const_iterator
-    cend() const {
-        return {value + _size};
+    cend() const
+    {
+        return { value + _size };
     }
 
     [[nodiscard]] reverse_iterator
-    rbegin() const {
-        return {value + _size - 1};
+    rbegin() const
+    {
+        return { value + _size - 1 };
     }
 
     [[nodiscard]] reverse_iterator
-    rend() const {
-        return {value};
+    rend() const
+    {
+        return { value };
     }
 
     [[nodiscard]] const_reverse_iterator
-    crbegin() const {
-        return {value + _size - 1};
+    crbegin() const
+    {
+        return { value + _size - 1 };
     }
 
     [[nodiscard]] const_reverse_iterator
-    crend() const {
-        return {value};
+    crend() const
+    {
+        return { value };
     }
 };
