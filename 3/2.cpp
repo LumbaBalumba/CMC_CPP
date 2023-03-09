9cd9706144f50429189047df4728cd9d5329a51e
i3alumba <vlad.lazar.ik528@yandex.ru>
2023-03-09 18:01:56 +0300

    3-2

--- 3/2.cpp
+++ 3/2.cpp
@@ -1,142 +1,50 @@
-#include <string>
 #include <cmath>
-#include <sstream>
-#include <iomanip>
+#include <iostream>
+#include <complex>
+
+typedef std::complex<double> complex;
 
 namespace numbers
 {
-    class complex
-    {
-        double _re, _im;
-    public:
-        complex(double re = 0, double im = 0) : _re(re), _im(im)
-        {}
-
-        explicit complex(const std::string &other)
-        {
-            _re = _im = 0;
-            const char *str = other.c_str();
-            sscanf(str, "(%lf,%lf)", &_re, &_im);
-        }
-
-        [[nodiscard]] double
-        re() const
-        {
-            return _re;
-        }
-
-        [[nodiscard]] double
-        im() const
-        {
-            return _im;
-        }
-
-        [[nodiscard]] double
-        abs2() const
-        {
-            return _im * _im + _re * _re;
-        }
-
-        [[nodiscard]] double
-        abs() const
-        {
-            return sqrt(_im * _im + _re * _re);
-        }
-
-        [[nodiscard]] std::string
-        to_string() const
-        {
-            std::stringstream tmp;
-            tmp << std::setprecision(10) << "(" << _re << "," << _im << ")";
-            return tmp.str();
-        }
-
-        complex &
-        operator+=(const complex &other)
-        {
-            return *this = *this + other;
-        }
-
-        complex &
-        operator-=(const complex &other)
-        {
-            return *this = *this - other;
-        }
-
-        complex &
-        operator*=(const complex &other)
-        {
-            return *this = *this * other;
-        }
-
-        complex &
-        operator/=(const complex &other)
-        {
-            return *this = *this / other;
-        }
-
-        friend complex
-        operator+(const complex &left, const complex &right)
-        {
-            return { left._re + right._re, left._im + right._im };
-        }
-
-        friend complex
-        operator-(const complex &left, const complex &right)
-        {
-            return { left._re - right._re, left._im - right._im };
-        }
-
-        friend complex
-        operator*(const complex &left, const complex &right)
-        {
-            return { left._re * right._re - left._im * right._im, left._im * right._re + left._re * right._im };
-        }
-
-        complex
-        operator~() const
-        {
-            return { _re, -_im };
-        }
-
-        friend complex
-        operator/(const complex &left, double right)
-        {
-            return { left._re / right, left._im / right };
-        }
-
-        friend complex
-        operator/(const complex &left, const complex &right)
-        {
-            return left * ~right / right.abs2();
-        }
-
-        complex
-        operator-() const
-        {
-            return { -_re, -_im };
-        }
-    };
-
     class complex_stack
     {
         complex *_arr;
         size_t _cap, _size;
 
     public:
-        complex_stack() : _arr((complex *) calloc(1, sizeof(complex))), _cap(0), _size(1)
+        complex_stack() : _arr((complex *) calloc(1, sizeof(complex))), _cap(1), _size(0)
         {}
 
-        complex_stack(size_t size) : _arr((complex *) calloc(size, sizeof(complex))), _cap(size), _size(size)
+        explicit complex_stack(size_t size) : _arr((complex *) calloc(size, sizeof(complex))), _cap(size), _size(size)
         {}
 
-        complex_stack(const complex_stack &other) : _arr((complex *) calloc(other._size, sizeof(complex))),
+        complex_stack(const complex_stack &other) : _arr((complex *) calloc(other._cap, sizeof(complex))),
                                                     _cap(other._cap), _size(other._size)
-        {}
+        {
+            for (size_t i = 0; i < _size; ++i) {
+                _arr[i] = other._arr[i];
+            }
+        }
 
-        complex_stack(complex_stack &&other) : _arr(other._arr), _cap(other._cap), _size(other._size)
+        complex_stack(complex_stack &&other) noexcept: _arr(other._arr), _cap(other._cap), _size(other._size)
         {
             other._arr = nullptr;
+        }
+
+        complex_stack &
+        operator=(const complex_stack &other)
+        {
+            if (this == &other) {
+                return *this;
+            }
+            free(_arr);
+            _arr = (complex *) calloc(other._cap, sizeof(complex));
+            _cap = other._cap;
+            _size = other._size;
+            for (size_t i = 0; i < _size; ++i) {
+                _arr[i] = other._arr[i];
+            }
+            return *this;
         }
 
         [[nodiscard]] size_t
@@ -163,9 +71,8 @@ namespace numbers
         }
 
         friend complex_stack
-        operator<<(const complex_stack &left, const complex &value)
+        operator<<(complex_stack stack, const complex &value)
         {
-            complex_stack stack = left;
             if (stack._size == stack._cap) {
                 stack._arr = (complex *) realloc(stack._arr, sizeof(complex) * (stack._cap *= 2));
             }
@@ -184,8 +91,6 @@ namespace numbers
         {
             complex_stack res = *this;
             res._size--;
-            res._cap = res._size;
-            res._arr = (complex *) realloc(res._arr, res._cap);
             return res;
         }
     };
