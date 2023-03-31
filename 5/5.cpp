a36faaa8fcbbe8fb821f8df5e8f3acbc9459584d
i3alumba <vlad.lazar.ik528@yandex.ru>
2023-03-31 13:06:04 +0300

    5-5

--- 5/5.cpp
+++ 5/5.cpp
@@ -1,42 +1,92 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <map>
+#include <cinttypes>
 
-constexpr uint64_t mod = 4294967161;
+constexpr uint64_t MOD = 4294967161;
+
+template<uint64_t Mod>
+class Congruence
+{
+    uint64_t value;
+public:
+    Congruence() = default;
+
+    Congruence(uint64_t value) : value(value % Mod)
+    {}
+
+    Congruence(const Congruence &other) = default;
+
+    Congruence(Congruence &&other) = default;
+
+    Congruence &
+    operator=(const Congruence &other) = default;
+
+    friend Congruence
+    operator+(const Congruence &left, const Congruence &right)
+    {
+        return { left.value + right.value };
+    }
+
+    Congruence &
+    operator+=(const Congruence &other)
+    {
+        return *this = *this + other;
+    }
+
+    friend Congruence
+    operator*(const Congruence &left, const Congruence &right)
+    {
+        return { left.value * right.value };
+    }
+
+    Congruence &
+    operator*=(const Congruence &other)
+    {
+        return *this = *this * other;
+    }
+
+    friend std::ostream &
+    operator<<(std::ostream &out, const Congruence &c)
+    {
+        return out << c.value;
+    }
+
+    bool
+    operator==(const Congruence &other) const = default;
+
+    auto
+    operator<=>(const Congruence &other) const = default;
+};
+
+std::map<std::pair<uint32_t, uint32_t>, Congruence<MOD>>
+matrix_mul(
+        const std::map<std::pair<uint32_t, uint32_t>, Congruence<MOD>> &left,
+        const std::map<std::pair<uint32_t, uint32_t>, Congruence<MOD>> &right)
+{
+    std::map<std::pair<uint32_t, uint32_t>, Congruence<MOD>> res;
+    //  Magic matrices operations
+    return res;
+}
 
 int
 main()
 {
-    std::map<uint32_t, std::map<uint32_t, uint64_t>> fir;
-    uint32_t i, j;
-    uint64_t val;
-    while (std::cin >> i >> j >> val) {
-        if (!i && !j && val == mod) {
+    std::map<std::pair<uint32_t, uint32_t>, Congruence<MOD>> matrix1, matrix2, res;
+    uint32_t row, col, value;
+    while (std::cin >> row >> col >> value) {
+        if (row == 0 && col == 0 && value == MOD) {
             break;
         }
-        fir[i][j] = val;
+        matrix1[{ row, col }] = { value };
     }
-    std::map<uint32_t, std::map<uint32_t, uint64_t>> sec;
-    while (std::cin >> i >> j >> val) {
-        sec[j][i] = val;
+    res = matrix_mul(matrix1, matrix2);
+    while (std::cin >> row >> col >> value) {
+        matrix2[{ row, col }] = { value };
     }
 
-    std::map<uint32_t, std::map<uint32_t, uint64_t>> ans;
-    for (auto &el: fir) {
-        for (auto &se: sec) {
-            uint64_t res = 0;
-            for (auto &num: el.second) {
-                res = (res + num.second * se.second[num.first] % mod) % mod;
-            }
-            if (res % mod) {
-                ans[el.first][se.first] = res % mod;
-            }
-        }
-    }
-    for (auto &st: ans) {
-        for (auto &num: st.second) {
-            std::cout << st.first << " " << num.first << " " << num.second << std::endl;
+    for (auto &elem: res) {
+        if (elem.second != 0) {
+            std::cout << elem.first.first << " " << elem.first.second << " " << elem.second << std::endl;
         }
     }
 }
