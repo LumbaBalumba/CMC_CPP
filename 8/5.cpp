3ff6e050b75a23a96dfe7aca98870f2c4b849902
i3alumba <vlad.lazar.ik528@yandex.ru>
2023-04-12 12:56:09 +0300

    8-5

--- 8/5.cpp
+++ 8/5.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <chrono>
 
-int main() {
-    std::cout << "Hello world" << std::endl;
+
+int
+main()
+{
+    const char *fstr = "%Y-%m-%d";
+    uint64_t res = 0;
+    std::tm tm{};
+    std::string date;
+    std::cin >> date;
+    strptime(date.c_str(), fstr, &tm);
+    auto p1 = std::chrono::system_clock::from_time_t(std::mktime(&tm));
+    while (std::cin >> date) {
+        strptime(date.c_str(), fstr, &tm);
+        auto p2 = std::chrono::system_clock::from_time_t(std::mktime(&tm));
+        auto elapsed = std::chrono::duration_cast<std::chrono::hours>(p2 - p1);
+        res += std::abs(elapsed.count()) / 24;
+        p1 = p2;
+    }
+    std::cout << res << std::endl;
 }
