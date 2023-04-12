37c071cff02679b9594ae1743cb9d5ea0d129d94
i3alumba <vlad.lazar.ik528@yandex.ru>
2023-04-12 17:00:02 +0300

    Codestyle fixes

--- 8/5.cpp
+++ 8/5.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <chrono>
 
+enum
+{
+    DAY_HOURS = 24
+};
+
 
 int
 main()
@@ -16,7 +21,7 @@ main()
         strptime(date.c_str(), fstr, &tm);
         auto p2 = std::chrono::system_clock::from_time_t(std::mktime(&tm));
         auto elapsed = std::chrono::duration_cast<std::chrono::hours>(p2 - p1);
-        res += std::abs(elapsed.count()) / 24;
+        res += std::abs(elapsed.count()) / DAY_HOURS;
         p1 = p2;
     }
     std::cout << res << std::endl;
